ffbe032fd76444d59c6a762522c883047e9eee67
Oladimeji Williams <oladimejiwilliams@outlook.com>
2025-06-19 03:17:50 +0100

    updated the base url

--- src/app/services/transaction.ts
+++ src/app/services/transaction.ts
@@ -8,7 +8,7 @@ import { Transaction } from '../models/transaction';
   providedIn: 'root'
 })
 export class TransactionService {
-  private apiUrl = 'http://expensesappapi-gbgze6exfwe6gcad.germanywestcentral-01.azurewebsites.net/api/Transaction';
+  private apiUrl = 'https://expensesappapi-gbgze6exfwe6gcad.germanywestcentral-01.azurewebsites.net/api/Transaction';
 
 
   constructor(private http: HttpClient) {}
